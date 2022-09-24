369553cacb2df9bfb4bf2b4bbbb27d4cb6954805
Giuseppe Portelli <gportelli@Giuseppes-Air.homenet.telecomitalia.it>
2022-09-24 17:45:48 +0200

    first commit

---
+++ htdocs/giosueario.lib.php
@@ -0,0 +1,136 @@
+<?php
+class Giosueario {
+	private function getCurrY() {
+		return empty($_GET['year']) ? date("Y") : $_GET['year'];
+	}
+	
+	private function getCurrM() {
+		return empty($_GET['month']) ? date("n") : $_GET['month'];
+	}
+	
+	public function getYearSelect() {
+		$currY = $this->getCurrY();
+		$currM = $this->getCurrM();
+				
+		echo "<input type='hidden' id='offset' value='" . $this->getOffset() . "'/>";
+		
+		echo "<a href='?offset=" . $this->getOffset() . "&year=" . ($currY-1) . "&month=$currM'>&laquo</a> ";
+		
+		echo "<select id='year' name='year' onChange='update()'>";
+		
+		for($i=$currY - 5; $i<$currY + 5; $i++)
+			echo "<option value='$i' " . ($i == $currY ? "selected='selected'" : ""). ">$i</option>";
+		
+		echo "</select>";
+		
+		echo " <a href='?offset=" . $this->getOffset() . "&year=" . ($currY+1) . "&month=$currM'>&raquo</a> ";
+	}
+	
+	public function getMonthSelect() {
+		$months = array(
+				"Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"
+		);
+	
+		$currY = $this->getCurrY();
+		$currM = $this->getCurrM();
+		
+		$nextM = $currM +1;
+		$prevM = $currM -1;
+		
+		$nextY = $currY;
+		$prevY = $currY;
+		
+		if($prevM == 0) {
+			$prevM = 12;
+			$prevY--;
+		}
+		
+		if($nextM == 13) {
+			$nextM = 1;
+			$nextY++;
+		}
+		
+		echo "<a href='?offset=" . $this->getOffset() . "&year=$prevY&month=$prevM'>&laquo</a> ";
+		
+		echo "<select id='month' name='month' onChange='update()'>";
+	
+		for($i=0; $i<12; $i++)
+			echo "<option value='" . ($i+1) . "' " . ($i+1 == $currM ? "selected='selected'" : ""). ">" . $months[$i] . "</option>";
+	
+		echo "</select>";
+		
+		echo " <a href='?offset=" . $this->getOffset() . "&year=$nextY&month=$nextM'>&raquo</a> ";
+	}
+	
+	public function drawCalendar() {
+		$year = $this->getCurrY();
+		$month = $this->getCurrM();
+		
+		$startDay = mktime(1, 0, 0, $month, 1, $year);
+		$wd = date("w", $startDay);
+		if($wd == 0) $wd = 6; else $wd = $wd - 1;
+		$startDay -= $wd * 3600 * 24;
+		
+		$days = array("Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato", "Domenica");
+		
+		echo "<table cellspacing='1' cellpadding='4' width='100%'>";
+		
+		$day = $startDay;
+		for($i=0; $i<7; $i++) {
+			echo "<tr>";
+			
+			for($j=0; $j<7; $j++) {
+				if($i == 0)
+					echo "<th>" . $days[$j] . "</th>";
+				else {
+					$style = date("n", $day) == $month ? "currMonth" : "otherMonth";
+					if(date("j", $day) == date("j") && date("n", $day) == date("n") && date("Y", $day) == date("Y")) {
+						$style = 'currDay';
+					}
+					echo "<td class='$style'>";
+					echo date("j", $day);
+					echo "<br/>";
+					echo $this->getTurno($day);
+					echo "</td>";				
+					$day += 3600 * 24;
+				}
+			}
+			
+			echo "</tr>";
+		}
+			
+		echo "</table>";
+	}
+	
+	private function getTurno($day) {
+		$turni = array("Sera", "Pomeriggio", "Mattina", "Notte", "Riposo", "Fuori turno", "Aggiornamento");
+		$refDay = mktime(1, 0, 0, 3, 10, 2014); // lunedì - sera - inizio ciclo di 7 turni in quinta + 2 settimane fuori turno = 5 * 7 + 7 * 2 = 49 giorni
+		$delta  =  round(($day - $refDay) / 3600 / 24); // Attenzione all'ora legale, che sfalsa gli offset di 1h
+		$offset = $this->getOffset();
+		
+		if($offset === "") {
+			$a = $delta % 49;
+            if($a < 0) $a += 49;
+			if($a < 35) $t = $a % 5; else $t = 5;
+		}
+		else $t = ($delta + $offset) % 5;
+		
+		if($t<0) $t += 5;
+		
+		//echo date('w', $day);
+		if($t == 4 && date('w', $day) == 2) $t = 6; // Se il riposo cade di martedì si fa aggiornamento
+		
+		return $turni[$t];
+	}
+	
+	public function getOffset() {
+		if(! isset($_GET['offset'])) return "";
+		if($_GET['offset'] === "") return "";
+		
+		$values = array(0, 1, 2, 3, 4);
+		
+		if(! in_array($_GET['offset'], $values)) return "";
+		
+		return (int) $_GET['offset'];
+	}
+}
---
+++ htdocs/index.php
@@ -0,0 +1,80 @@
+<?php
+include 'giosueario.lib.php';
+
+$g = new Giosueario();
+?>
+<!doctype html>
+<html>
+<head>
+	<title>Il Giosueario</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<style>
+		body { font-size: 14; font-family: Arial; margin: 5px; }
+		h1 {margin-bottom: 0px; margin-top: 0px;}
+		h3 {margin-bottom: 3px; margin-top: 5px;}
+		a, a:visited { text-decoration: none; color: red; }
+		a:hover { text-decoration: none; color: blue; }
+		.currMonth {}
+		.otherMonth { color: #888; }
+		td { background-color: #d8d8d8; }
+		th { background-color: #d6f680; }
+		.currDay { font-weight: bold; color: blue; background-color: #86e652;}
+	</style>
+	<script>
+		function update() {
+			var y = document.getElementById('year').value;
+			var m = document.getElementById('month').value;
+			var o = document.getElementById('offset').value;
+
+			document.location.href = "?offset=" + o + "&year=" + y + "&month=" + m;
+		}
+	</script>
+</head>
+<body>
+	<div style="text-align: center">
+	
+		<h1><?php 
+			if($g->getOffset() === "")
+				echo "Il Giosueario";
+			else
+				echo "Turni in quinta (ciclo " . chr(ord("A") + $g->getOffset()) . ")";
+		?></h1>
+	
+		<p>
+			[ <a href="?">giosueario</a> | 
+			<a href="?offset=0">turni a</a> | 
+			<a href="?offset=1">turni b</a> | 
+			<a href="?offset=2">turni c</a> | 
+			<a href="?offset=3">turni d</a> | 
+			<a href="?offset=4">turni e</a> ]
+		</p>
+		
+		<a href="?offset=<?php echo $g->getOffset()?>&month=&year=">reset</a><br/>
+		<?php $g->getYearSelect(); ?> 
+		<br/>
+		<?php $g->getMonthSelect(); ?>
+		
+		
+		<div style="margin: auto; width:80%; margin-top: 4px; margin-bottom: 8px;">
+		<?php $g->drawCalendar(); ?>
+		</div>
+		
+		<div style="margin:auto; width:400px">
+			<table width="100%" cellpadding="4" cellspacing="1">
+				<tr>
+					<th colspan='2'>
+						Legenda Turni
+					</th>
+				</tr>
+				
+				<tr><td>Mattina</td><td>07 - 13</td></tr>
+				<tr><td>Pomeriggio</td><td>13 - 19</td></tr>
+				<tr><td>Sera</td><td>19 - 24</td></tr>
+				<tr><td>Notte</td><td>00 - 07</td></tr>
+				<tr><td>Aggiornamento</td><td>08 - 14</td></tr>
+				<tr><td>Fuori turno</td><td>Jolly</td></tr>
+			</table>
+		</div>
+	</div>
+</body>
+</html>
